0b4d1121415c0063d0ef79b084612d2602df93ea
Balazs9 <sempergerbalazs@gmail.com>
2022-01-10 20:40:33 +0000

    fix typing error

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yummy-booking-syytem.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["yummy-booking-system.herokuapp.com", "localhost"]
 
 
 # Application definition
