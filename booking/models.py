eb16c4e0a2432b61d0eb3e14d7f91620a19bd739
Balazs9 <sempergerbalazs@gmail.com>
2022-01-11 17:38:09 +0000

    add guest, table and booking class in models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Guest(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+
+class Table(models.Model):
+    table = models.IntegerField()
+    max_people = models.IntegerField()
+
+    def table_counter(self):
+        "Returns the table options"
+        if self.min_people < table:
+            return "This table is only for 2 people"
+        elif self.min_people < table:
+            return "Table for 4 but maximum 6 people"
+        else:
+            return "Please contact us to arrange bigger event"
+
+class Booking(models.Model):
+    table_options = models.ForeignKey('Table', on_delete=models.CASCADE)
+    guest_list = models.ForeignKey('Guest', on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def reservation(self):
+        if table_options == table_counter:
+            return "Booked"
+        else:
+            return "Please check an other option"
+    
+
+
