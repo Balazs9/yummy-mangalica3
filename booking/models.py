3bc992c7abe71218bb768e8218278220eebd2ce0
Balazs9 <sempergerbalazs@gmail.com>
2022-01-11 22:09:46 +0000

    add templates files

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 
 class Guest(models.Model):
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Guest, Table, Booking
 
-# Create your views here.
+
