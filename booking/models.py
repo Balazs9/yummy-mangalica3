1942e65aaa4df5af736eb4a13c546bb6fc7b3f96
Balazs9 <sempergerbalazs@gmail.com>
2022-01-11 17:40:31 +0000

    fixing typing error

--- booking/models.py
+++ booking/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 class Guest(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)
+
 
 class Table(models.Model):
     table = models.IntegerField()
@@ -13,12 +15,13 @@ class Table(models.Model):
 
     def table_counter(self):
         "Returns the table options"
-        if self.min_people < table:
+        if self.max_people < table:
             return "This table is only for 2 people"
-        elif self.min_people < table:
+        elif self.max_people < table:
             return "Table for 4 but maximum 6 people"
         else:
             return "Please contact us to arrange bigger event"
+
 
 class Booking(models.Model):
     table_options = models.ForeignKey('Table', on_delete=models.CASCADE)
